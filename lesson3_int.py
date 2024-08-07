1faeb3603c3cdb3559eada45b6673738cf56bcda
alyrden <f50knox@gmail.com>
2024-08-07 23:54:50 +0300

    Add files via upload

---
+++ lesson3_int.py
@@ -0,0 +1,6 @@
+dz =12
+hour = 1.5
+name_kurs = "Python"
+time_mission = 1.5 / 12
+
+print("Курс: ", name_kurs, ", всего задач", dz, ", затрачено часов:", hour, ", среднее время выполнения", time_mission)
